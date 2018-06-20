2f4d7f2718ccde23cd441bfb7d6af569988d4712
yangwliu <yangwliu@thoughtworks.com>
2018-06-20 15:02:25 +0800

    [LiuYang] feat: add pages for homework details.

--- app.js
+++ app.js
@@ -1,14 +1,39 @@
-//app.js
 App({
   onLaunch: function () {
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
-
+    var that = this;
     // 登录
     wx.login({
       success: res => {
+        if (res.code) {
+          console.log('code:' + res.code)
+          wx.request({
+            url: 'http://localhost:5678/pweb/openId',
+            data: {
+              jsCode: res.code
+            },
+            method: 'GET',
+            success: function (res) {
+              that.globalData.openId = res.data.openId
+
+              if (res.data.interviewerName) {
+                that.globalData.interviewerName = res.data.interviewerName;
+                that.globalData.interviewerId = res.data.interviewerId
+              }
+
+              if (that.openIdCallback) {
+                that.openIdCallback(res.data)
+              }
+              console.log('openId: ' + that.globalData.openId)
+            }
+
+          })
+        } else {
+          console.log('登陆失败： ' + res.errMsg)
+        }
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
       }
     })
@@ -20,13 +45,16 @@ App({
           wx.getUserInfo({
             success: res => {
               // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
+              console.log('here is userInfo:' + res.userInfo)
+              that.globalData.userInfo = res.userInfo
 
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
               // 所以此处加入 callback 以防止这种情况
               if (this.userInfoReadyCallback) {
                 this.userInfoReadyCallback(res)
               }
+
+              console.log('userInfo:' + that.globalData.userInfo)
             }
           })
         }
@@ -34,6 +62,9 @@ App({
     })
   },
   globalData: {
-    userInfo: null
+    userInfo: null,
+    interviewerName: null,
+    interviewerId: null,
+    openId: null,
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,170 @@
 //index.js
 //获取应用实例
 const app = getApp()
-
+var util = require('../../utils/util.js');
 Page({
   data: {
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    unfinishedHomework: [],
+    finishNum: 0,
+    openId: '',
+    interviewerName: '',
+    interviewerId: '',
+    currentYear: null,
   },
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function () {
     wx.navigateTo({
       url: '../logs/logs'
     })
   },
+
+  getHomeWorks: function (interviewerId, that) {
+    wx.request({
+      url: 'http://localhost:5678/pweb/assignment/' + interviewerId,
+      success: function (res) {
+        console.log(res)
+        console.log('response:' + res.data.numberOfFinished);
+        var unfinishedHomeworks = [];
+        for (var i = 0; i < res.data.unfinished.length; i++) {
+          var item = res.data.unfinished[i]
+          var single_homework = {}
+          single_homework.candidate_name = item.candidate_name
+          single_homework.assigned_date = that.getDateString(item.assigned_date)
+          single_homework.deadline_date = that.getDateString(item.deadline_date)
+          single_homework.remain_hours = that.getRemainHours(item.deadline_date)
+          single_homework.remain_time_str = that.getRemainTimeStr(item.deadline_date)
+          unfinishedHomeworks[i] = single_homework
+        }
+
+        unfinishedHomeworks.sort(function (object1, object2) {
+          var val_1 = object1.remain_hours;
+          var val_2 = object2.remain_hours;
+
+          if (val_1 < val_2) {
+            return -1;
+          }
+          if (val_1 == val_2) {
+            return 0;
+          }
+          if (val_1 > val_2) {
+            return 1;
+          }
+        })
+
+        that.setData({
+          finishNum: res.data.numberOfFinished,
+          unfinishedHomework: unfinishedHomeworks,
+        })
+        console.log('interviewer: ' + that.data.finishNum)
+      }
+    })
+  },
+
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
+    var that = this
+
+    that.setData({
+      currentYear: that.getCurrentYear()
+    });
+
+    if (app.globalData.openId && app.globalData.openId != '') {
+      that.setData({
+        openId: app.globalData.openId,
+        interviewerName: app.globalData.interviewerName,
+        interviewerId: app.globalData.interviewerId,
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
+      });
+      that.getHomeWorks(that.data.interviewerId, that)
+    } else if (app.globalData.openId == null || app.globalData.openId == '') {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
+      // app.UserInfoReadyCallback = res => {
+      //   that.setData({
+      //     userInfo: res.userInfo,
+      //     hasUserInfo: true
+      //   })
+      // }
+
+      app.openIdCallback = data => {
+        console.log('openidcallback:' + data)
+        that.setData({
+          openId: data.openId,
+          interviewerName: data.interviewerName,
+          interviewerId: data.interviewerId,
+        });
+        that.getHomeWorks(that.data.interviewerId, that)
       }
     } else {
       // 在没有 open-type=getUserInfo 版本的兼容处理
       wx.getUserInfo({
         success: res => {
           app.globalData.userInfo = res.userInfo
-          this.setData({
+          that.setData({
             userInfo: res.userInfo,
             hasUserInfo: true
           })
         }
       })
     }
+
+    console.log('id:' + this.data.interviewerId)
   },
-  getUserInfo: function(e) {
-    console.log(e)
+
+  getUserInfo: function (e) {
+    console.log('getUserInfo:' + e.detail.userInfo)
     app.globalData.userInfo = e.detail.userInfo
+
     this.setData({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
+    console.log('userInfo: ' + this.data.userInfo.nickName)
+
+  },
+  getInterviewInfo: function (openId) {
+    wx.request({
+      url: 'localhost:5678/',
+    })
+  },
+  isOutOfTime: function (finishTime) {
+    var now = util.formatTime(new Date());
+    return now < finishTime
+  },
+
+  getRemainHours: function (deadline_date) {
+    var now = new Date();
+    var deadline = new Date(deadline_date);
+    var secs = deadline.getTime() - now.getTime();
+
+    var hours = secs/(1000*3600);
+    var result = Math.round(hours * 10)/10;
+    return result;
+  },
+
+  getRemainTimeStr: function(deadline_date) {
+    var now = new Date();
+    var deadline = new Date(deadline_date);
+    var secs = deadline.getTime() - now.getTime();
+
+    var hours = parseInt(secs/(1000*3600))
+    var mins = Math.round((secs%(1000*3600))/(1000*60))
+
+    return hours.toString() + "hour " + mins.toString() + "min"; 
+  },
+
+  getDateString: function(raw_date_string) {
+    var date = new Date(raw_date_string)
+    var date_string = util.formatTime(date)
+    return date_string
+  },
+
+  getCurrentYear: function() {
+    var date = new Date()
+    return date.getFullYear()
   }
 })
